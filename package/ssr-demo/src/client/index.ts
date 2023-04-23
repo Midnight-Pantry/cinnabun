c1fa7935021dcafb7075fdd582f7ddd093c76733
Robby6Strings <rausten93@gmail.com>
2023-04-23 14:18:57 +1200

    Add necessary files for SSR demo

--- package/ssr-demo/src/client/index.ts
+++ package/ssr-demo/src/client/index.ts
@@ -1,7 +1,7 @@
 import { Cinnabun } from "cinnabun"
 import { App } from "../App"
 import { SSRProps } from "cinnabun/src/types"
-import "./public/style.css"
+import "./index.css"
 
 if ("__cbData" in window) {
   Cinnabun.hydrate(App(), window.__cbData as SSRProps)
