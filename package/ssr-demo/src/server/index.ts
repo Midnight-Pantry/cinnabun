651bc3b117a88741d516c2844c49ba523147c515
Robby6Strings <rausten93@gmail.com>
2023-04-27 03:38:44 +1200

    rewriting server with fastify

--- package/ssr-demo/src/server/index.ts
+++ package/ssr-demo/src/server/index.ts
@@ -1,15 +1,10 @@
-import express from "express"
+import fastify from "fastify"
+import compress from "@fastify/compress"
+import fStatic from "@fastify/static"
+import websocket from "@fastify/websocket"
 import fs from "fs"
 import path from "path"
-import compression from "compression"
-import session from "express-session"
-import bodyParser from "body-parser"
-import { createServer } from "http"
 
-import * as passport from "passport"
-import { Strategy as LocalStrategy } from "passport-local"
-
-import { UserService, clearInvalidCookie, useAuth } from "./auth"
 import { ChatMessages } from "./chat"
 import { SSR } from "cinnabun/ssr"
 import { App } from "../App"
@@ -18,9 +13,9 @@ import { sleep } from "cinnabun/utils"
 
 const rootId = "app"
 
-const PORT = process.env.PORT || 3000
-const publicDir = path.resolve(__dirname, ".", "../../dist/static")
+const port: number = parseInt(process.env.PORT ?? "3000")
 let baseHtml = ""
+
 // load base html template
 {
   fs.readFile(
@@ -35,138 +30,76 @@ let baseHtml = ""
   )
 }
 
-const app = express()
-const server = createServer(app)
+const app = fastify()
+app.register(websocket, {
+  options: { maxPayload: 1048576 },
+})
+app.register(compress, { global: false })
+app.register(fStatic, {
+  prefix: "/static/",
+  root: path.join(__dirname, "../../dist/static"),
+})
+app.get("/favicon.ico", (_, res) => {
+  res.status(404).send()
+})
 
-// express app config
-{
-  app.use(compression())
-  app.use(bodyParser.json())
-  app.use(
-    session({
-      secret: "super-secret-secret",
-      saveUninitialized: true,
-      resave: true,
-      cookie: {
-        httpOnly: false,
-      },
-    })
-  )
+// app.register(async function (app) {
+//   app.get("/*", { websocket: true }, (connection) => {
+//     connection.socket.on("message", (message: string) => {
+//       console.log("got message - /*", message)
+//       connection.socket.send("hi from server")
+//     })
+//   })
+//   app.get("/", { websocket: true }, (connection) => {
+//     connection.socket.on("message", (message: string) => {
+//       console.log("got message - /", message)
+//       connection.socket.send("hi from server")
+//     })
+//   })
+// })
 
-  app.use(passport.initialize())
-  app.use(passport.session())
+app.get("/messages", async (_, res) => {
+  await sleep(100)
+  return { messages: ChatMessages.getAll() }
+})
 
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: "username",
-        passwordField: "password",
-      },
-      function (username, password, done) {
-        const user = UserService.get(username)
-        if (!user) return done(null, false)
-        if (!UserService.verifyPassword(user, password))
-          return done(null, false)
-        return done(null, { username })
-      }
-    )
-  )
-
-  passport.serializeUser(function (user, done) {
-    const res = user && "username" in user ? user.username : undefined
-    done(null, res)
-  })
-  passport.deserializeUser(function (id, done) {
-    const user = UserService.get(id as string)
-    done(null, user)
-  })
-  app.use(clearInvalidCookie)
-
-  app.use("/static", express.static(publicDir))
-  app.get("/favicon.ico", (_, res) => {
-    res.status(404).send()
-  })
-}
-
-// routes
-{
-  app.post(
-    "/login",
-    passport.authenticate("local", { passReqToCallback: true }),
-    (req, res) => {
-      res.cookie("user", JSON.stringify(req.user))
-      res.status(200).send(req.user)
-    }
-  )
-  app.post("/logout", (req, res) => {
-    try {
-      res.clearCookie("user")
-      req.session.destroy((err) => {
-        if (err) throw new Error(err)
-      })
-    } catch (error) {
-      return res.status(500).send()
-    }
-    return res.status(200).send()
-  })
-
-  app.get("/messages", async (_, res) => {
-    await sleep(100)
-    return res.json({ messages: ChatMessages.getAll() })
-  })
-  app.post("/message", useAuth, (req, res) => {
-    const { user } = req
-    if (!user) return res.status(403).send()
-    const username = (("username" in user && user.username) as string) ?? ""
-    if (!username) return res.status(403).send()
-    const { message } = req.body
-
-    return res
-      .status(200)
-      .send({ result: ChatMessages.create(message, username) })
-  })
-
-  app.get("/protected", useAuth, (req, res) => {
-    console.log("/protected", req.isAuthenticated())
-    res.send("shamow")
-  })
-}
-
-app.get(/.*/, async (req, res) => {
+app.get("/*", async (req, res) => {
   console.time("render time")
-
   const instance = new Cinnabun()
   instance.serverRequest = {
-    path: req.path,
-    data: { user: req.user },
+    path: req.url,
+    data: { user: null },
   }
 
   const { html, componentTree } = await SSR.serverBake(App(), instance)
   console.timeEnd("render time")
 
-  return res.send(
-    baseHtml
-      .replace(
-        `<div id="${rootId}"></div>`,
-        `<div id="${rootId}">${html}</div>`
-      )
-      .replace(
-        '<script id="server-props"></script>',
-        `<script id="server-props">
+  res
+    .code(200)
+    .header("Content-Type", "text/html; charset=utf-8")
+    .send(
+      baseHtml
+        .replace(
+          `<div id="${rootId}"></div>`,
+          `<div id="${rootId}">${html}</div>`
+        )
+        .replace(
+          '<script id="server-props"></script>',
+          `<script id="server-props">
           const root = document.getElementById('${rootId}');
           window.__cbData = {root, component: ${JSON.stringify(componentTree)}}
         </script>`
-      )
-  )
+        )
+    )
+  console.timeEnd("render time")
 })
 
-// app.use(
-//   express.static(path.resolve(__dirname, ".", "../../dist/public"), {
-//     maxAge: "30d",
-//   })
-// )
+app.listen({ port }, function (err, address) {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
+  console.log(`Server is listening on port ${port}`)
   console.log("http://localhost:3000")
 })
