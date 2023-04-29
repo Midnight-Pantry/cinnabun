3877b41ff5583809997c2b64d4b6431bca077270
Robby6Strings <rausten93@gmail.com>
2023-04-30 05:11:39 +1200

    mocha config / initial tests

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,2 +1,4 @@
 export * from "./src"
 export { Cinnabun, h } from "./src/cinnabun"
+
+//https://github.com/RTVision/mocha-esbuild
---
+++ packages/lib/src/component.domInterop.spec.ts
@@ -0,0 +1,72 @@
+import { JSDOM } from "jsdom"
+const { window } = new JSDOM()
+global.document = window.document
+global.window = global.document.defaultView!
+
+import { Component } from "."
+import { Cinnabun } from "./cinnabun"
+import { DomInterop } from "./domInterop"
+import { expect } from "chai"
+import "mocha"
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      document: Document
+      window: Window
+      navigator: Navigator
+    }
+  }
+}
+Object.assign(Cinnabun, { isClient: true })
+
+describe("When rendered, a component", () => {
+  beforeEach(() => {
+    const { window } = new JSDOM()
+    global.document = window.document
+    global.window = global.document.defaultView!
+  })
+
+  describe("with primitive children", () => {
+    const component = new Component("div", {
+      children: ["test", " ", 123],
+    })
+    const rendered = DomInterop.render(component)
+    const expected = "<div>test 123</div>"
+
+    it(`produces the HTML: ${expected}`, () => {
+      expect((rendered as HTMLDivElement).outerHTML).to.equal(expected)
+    })
+  })
+  describe("with primitive func children", () => {
+    const component = new Component("div", {
+      children: [() => "test", () => " ", () => 123],
+    })
+    const rendered = DomInterop.render(component)
+    const expected = "<div>test 123</div>"
+
+    it(`produces the HTML: ${expected}`, () => {
+      expect((rendered as HTMLDivElement).outerHTML).to.equal(expected)
+    })
+  })
+  describe("with componentFunc children", () => {
+    const component = new Component("div", {
+      children: [
+        () =>
+          new Component("p", {
+            children: ["test"],
+          }),
+        () =>
+          new Component("p", {
+            children: [123],
+          }),
+      ],
+    })
+    const rendered = DomInterop.render(component)
+    const expected = "<div><p>test</p><p>123</p></div>"
+
+    it(`produces the HTML: ${expected}`, () => {
+      expect((rendered as HTMLDivElement).outerHTML).to.equal(expected)
+    })
+  })
+})
