702c5b5a8f7b7f9fa24a883e73d001b466cb09dc
Robby6Strings <rausten93@gmail.com>
2023-04-30 22:41:22 +1200

    cli test commands improved

--- packages/lib/cypress/e2e/ssr/requestData.cy.js
+++ packages/lib/cypress/e2e/ssr/requestData.cy.js
@@ -1,15 +1,15 @@
 /// <reference types="cypress" />
 
+const port = Cypress.env("port")
+
 describe("requestData", () => {
   it("should display the correct route", async () => {
-    const port = Cypress.env("port")
     cy.request(`http://localhost:${port}/context`)
       .its("body")
       .should("include", '<button disabled="true">Subtract</button>')
   })
 
   it("should display the 'Log in' button when we're not authenticated", async () => {
-    const port = Cypress.env("port")
     cy.request(`http://localhost:${port}`)
       .its("body")
       .should("include", "<button>Log in</button>")
