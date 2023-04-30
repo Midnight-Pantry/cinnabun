334b9bc98e5a2925459116f70b6be2b69495a212
Robby6Strings <rausten93@gmail.com>
2023-05-01 00:05:36 +1200

    ssr auth test

--- packages/lib/cypress/e2e/ssr/requestData.cy.js
+++ packages/lib/cypress/e2e/ssr/requestData.cy.js
@@ -1,6 +1,14 @@
 /// <reference types="cypress" />
+const { generateUUID } = require("../util")
 
 const port = Cypress.env("port")
+
+const preAuth = async function () {
+  await cy.request("POST", `http://localhost:${port}/create-account`, {
+    username: generateUUID(),
+    password: "123",
+  })
+}
 
 describe("requestData", () => {
   it("should display the correct route", async () => {
@@ -9,9 +17,15 @@ describe("requestData", () => {
       .should("include", '<button disabled="true">Subtract</button>')
   })
 
-  it("should display the 'Log in' button when we're not authenticated", async () => {
+  it("should display the 'Log in' button when we aren't authenticated", async () => {
     cy.request(`http://localhost:${port}`)
       .its("body")
       .should("include", "<button>Log in</button>")
+  })
+  it("should display the 'Log out' button when we are authenticated", async () => {
+    await preAuth()
+    cy.request(`http://localhost:${port}`)
+      .its("body")
+      .should("include", "<button>Log out</button>")
   })
 })
---
+++ packages/lib/cypress/e2e/util.js
@@ -0,0 +1,43 @@
+const parseJwt = (token) => {
+  const base64Url = token.split(".")[1]
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/")
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
+      })
+      .join("")
+  )
+
+  return JSON.parse(jsonPayload)
+}
+
+const generateUUID = () => {
+  // Public Domain/MIT
+  var d = new Date().getTime() //Timestamp
+  var d2 =
+    (typeof performance !== "undefined" &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0 //Time in microseconds since page-load or 0 if unsupported
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0
+      d = Math.floor(d / 16)
+    } else {
+      //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0
+      d2 = Math.floor(d2 / 16)
+    }
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16)
+  })
+}
+
+module.exports = {
+  parseJwt,
+  generateUUID,
+}
