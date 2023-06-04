43455883e068b6f2da222e72383079be725186c5
Robby6Strings <rausten93@gmail.com>
2023-06-05 01:03:11 +1200

    portal.ts - now persists a record of portal instances and appends children when the same portal is referenced

--- packages/lib/src/portal.ts
+++ packages/lib/src/portal.ts
@@ -2,14 +2,26 @@ import { Cinnabun } from "./cinnabun"
 import { Component } from "./component"
 import { ComponentChildren } from "./types"
 
+const portalRoots: Record<string, Component> = {}
+
 export const createPortal = (children: ComponentChildren, rootId: string) => {
   if (!Cinnabun.isClient) return ""
+
+  if (portalRoots[rootId]) {
+    for (const c of children) {
+      if (!c) continue
+      portalRoots[rootId].appendChildren(c)
+    }
+    return portalRoots[rootId]
+  }
 
   const element = document.getElementById(rootId)
   if (!element) throw new Error(`Element with id ${rootId} not found`)
 
-  return Object.assign(new Component(element.tagName, { children }), {
+  const res = Object.assign(new Component(element.tagName, { children }), {
     element,
     isStatic: true,
   })
+  portalRoots[rootId] = res
+  return res
 }
