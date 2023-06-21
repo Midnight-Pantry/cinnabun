13b73a4af0d50116f66ce5e180072a1d10400ad1
Robby6Strings <rausten93@gmail.com>
2023-06-21 22:19:17 +1200

    cinnabun-transitions - Transition now uses 'useTransition'

--- packages/transitions/src/Transition.tsx
+++ packages/transitions/src/Transition.tsx
@@ -1,66 +1,22 @@
-import * as Cinnabun from "cinnabun"
+import { Component } from "cinnabun"
 import { PropsWithChildren } from "cinnabun/types"
 import { TransitionProps } from "./types"
+import { useTransition } from "./useTransition"
 
 export const Transition = ({
+  tag = "div",
   children,
   properties,
   ...rest
 }: PropsWithChildren & TransitionProps) => {
-  const fromStyle = (reverse: boolean = false) => {
-    return properties
-      .map((p) => `${p.name}: ${reverse ? `calc(-1 * ${p.from})` : p.from}`)
-      .join(";")
-  }
-  const toStyle = () => {
-    return properties.map((p) => `${p.name}: ${p.to}`).join(";")
-  }
-
-  const handleMount = (self: Cinnabun.Component) => {
-    if (!self.element || !Cinnabun.Cinnabun.isClient) return
-    self.element.setAttribute("style", `${transitionProps()};${toStyle()}`)
-  }
-  const handleBeforeUnmount = (self: Cinnabun.Component) => {
-    if (!self.element || !Cinnabun.Cinnabun.isClient) return false
-    const el = self.element
-    let exitStyle = ""
-    if (rest.absoluteExit) {
-      const currentPos = self.element.getBoundingClientRect()
-      const absolutePosStyle = `position:absolute;top:${currentPos.top}px;left:${currentPos.left}px;right:${currentPos.right}px;bottom:${currentPos.bottom}px;width:${currentPos.width}px;height:${currentPos.height}px;`
-      exitStyle += absolutePosStyle
-    }
-
-    el.setAttribute(
-      "style",
-      `${exitStyle}${transitionProps()};${fromStyle(rest.reverseExit)}`
-    )
-
-    // get the longest transition time
-    const longestTransition = properties.reduce((acc, curr) => {
-      if (curr.ms && curr.ms > acc) return curr.ms
-      return acc
-    }, 300)
-
-    return new Promise<boolean>((res) => {
-      setTimeout(() => {
-        return res(true)
-      }, longestTransition)
-    })
-  }
-
-  const transitionProps = () => {
-    return `transition: ${properties
-      .map((p) => `${p.name} ${p.ms ?? 300}ms`)
-      .join(",")}`
-  }
-  return (
-    <div
-      style={`${transitionProps()};${fromStyle()}`}
-      onMounted={handleMount}
-      onBeforeUnmounted={handleBeforeUnmount}
-      {...rest}
-    >
-      {children}
-    </div>
-  )
+  const { onMounted, onBeforeUnmounted, initialStyle } = useTransition({
+    properties,
+  })
+  return new Component(tag, {
+    children,
+    initialStyle,
+    onMounted,
+    onBeforeUnmounted,
+    ...rest,
+  })
 }
