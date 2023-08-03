902b04cd5798a008cd830aee6d2d9ca0b1771287
Robby6Strings <rausten93@gmail.com>
2023-08-04 03:37:05 +1200

    revert markup change to SignalsExample

--- apps/components/src/SignalsExample.tsx
+++ apps/components/src/SignalsExample.tsx
@@ -3,19 +3,16 @@ import { useComputed, useSignal } from "cinnabun"
 
 export const SignalsExample = () => {
   const [count, setCount] = useSignal(0)
-  const [count2, setCount2] = useSignal(0)
 
   useComputed(() => {
-    console.log("count changed", count.value + count2.value)
+    console.log("count changed", count.value)
     return 123
-  }, [count, count2])
+  }, [count])
 
   return (
     <>
       <h1>{count}</h1>
-      <h1>{count2}</h1>
       <button onclick={() => setCount((prev) => prev + 1)}>click me</button>
-      <button onclick={() => setCount2((prev) => prev + 1)}>click me</button>
       <br />
       <br />
       <input
